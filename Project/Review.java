bccfbeebb1ed7a3f2f4035d044fcb8fdfd32e794
JarrelNT <113711841+JarrelNT@users.noreply.github.com>
2022-10-25 13:23:44 +0800

    Update Review.java

--- Project/Review.java
+++ Project/Review.java
@@ -1,12 +1,37 @@
+
 public class Review {
 
 	private string reviewText;
 	private int Rating;
 	private string MovieName;
 
+    public Review(string reviewText, int Rating, string MovieName) {
+        this.reviewText = reviewText;
+        this.Rating = Rating;
+        this.MovieName = MovieName;
+    }
+
+    
+
 	public void getCustomer() {
 		// TODO - implement Review.getCustomer
 		throw new UnsupportedOperationException();
 	}
+
+    public int getRating() {
+		// TODO - implement Movie.getRating
+        return this.Rating;
+		
+	}
+
+    public string getReview() {
+		// TODO - implement Movie.getReview
+        return this.reviewText;
+	}
+    
+    public string getMovieName() {
+           
+        return this.MovieName;
+    }
 
 }
