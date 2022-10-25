511dc823367a24c78efd833709c9ae23bb6bb82d
kopikia <100225153+kopikia@users.noreply.github.com>
2022-10-25 17:16:49 +0800

    Update Customer.java

--- Project/Customer.java
+++ Project/Customer.java
@@ -1,6 +1,20 @@
-public class Customer {
+public class Customer extends User {
 
 	private String UserID;
 	private String typeOfCustomer;
 
+	public Customer(String Name, String Email, String Password, String typeOfCustomer) {
+		super(Name, Email, Password);
+		this.UserID=Email;
+		this.typeOfCustomer=typeOfCustomer;
+	}
+
+	public String getUserID() {
+		return this.UserID;
+	}
+	
+	public String getType() {
+		return this.typeOfCustomer;
+	}
+	
 }
