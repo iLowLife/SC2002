4623ac151cee567c27ca63a97488d2a1a9a7d37a
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-10-25 11:55:28 +0800

    updated

--- CoomVillage/User.java
+++ CoomVillage/User.java
@@ -1,8 +1,29 @@
+import java.util.*;
+
 public class User {
 
-	private string Name;
-	private string Email;
-	private string Password;
+	private String Name;
+	private String Email;
+	private String Password;
 	private boolean Role;
 
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Welcome to Coom Village!");
+		System.out.println("Name:");
+		String strName = sc.nextLine();
+		System.out.println("Email:");
+		String strMail = sc.nextLine();
+		System.out.println("Password:");
+		String strPW = sc.nextLine();
+		System.out.println("Role:");
+		String strRole = sc.nextLine();
+
+		if(strRole=="User")
+			Role="T";
+		else
+	}
 }
