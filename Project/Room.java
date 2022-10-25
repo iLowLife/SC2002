b196ac6768497ac93f0c8ef7bef16ade929854a9
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-10-25 13:01:48 +0800

    Update Room.java

--- Project/Room.java
+++ Project/Room.java
@@ -1,11 +1,21 @@
 //package MovieBooking;
 
 public class Room {
-	int roomNum;
+	private int roomNum;
+	int NumOfSeat = 25;
+	private Seat[] Seat = new Seat[NumOfSeat];
+
 	Room(int roomNum){
 		this.roomNum = roomNum;
+		for (int i=0; i<NumOfSeat; i++) {
+			this.getSeat()[i] = new Seat(i);
+		}
 	}
 	int getRoomNum() {
 		return this.roomNum;
+	}
+	public Seat[] getSeat() {
+		// TODO Auto-generated method stub
+		return Seat;
 	}
 }
