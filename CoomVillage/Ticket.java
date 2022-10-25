08948cccfff6b89b1118086c4048b8540f039d40
kopikia <100225153+kopikia@users.noreply.github.com>
2022-10-25 11:06:52 +0800

    Add files via upload

---
+++ CoomVillage/Ticket.java
@@ -0,0 +1,10 @@
+public class Ticket {
+
+	private int TicketID;
+	private string WeeklyDayPricing;
+	private string ConcessionPrice;
+	private string MovieSchedule;
+	private boolean Used;
+	private string UserEmail;
+
+}
--- CoomVillage/User.java
+++ CoomVillage/User.java
@@ -3,5 +3,6 @@ public class User {
 	private string Name;
 	private string Email;
 	private string Password;
+	private boolean Role;
 
 }
