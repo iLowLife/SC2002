e4941b808089a21a37021bbe70e2017b67c261c3
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-10-25 13:00:34 +0800

    Update Seat.java

--- Project/Seat.java
+++ Project/Seat.java
@@ -3,8 +3,20 @@
 public class Seat {
 	private boolean assigned;
 	private int SeatID;
-	
-	Seat(boolean assigned){
-		
+
+	Seat(int SeatID){
+		this.SeatID = SeatID;
+		this.assigned = false;
 	}
+
+	int getSeatID() {
+		return this.SeatID;
+	}
+	boolean getAssigned() {
+		return this.assigned;
+	}
+	void setAssigned(boolean assigned) {
+		this.assigned = assigned;
+	}
+	
 }
