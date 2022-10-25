caebfc82ed3e422dc116c1432032402f0a12e9e7
WilfredYYZ <113620820+WilfredYYZ@users.noreply.github.com>
2022-10-25 17:27:01 +0800

    Update Review.java

--- Project/Review.java
+++ Project/Review.java
@@ -1,37 +1,42 @@
-
+import java.util.*;
 public class Review {
 
-	private string reviewText;
+	private String reviewText;
 	private int Rating;
-	private string MovieName;
+	private String MovieName;
+	private String userID;
 
-    public Review(string reviewText, int Rating, string MovieName) {
+    public Review(String reviewText, int Rating, String MovieName, String userID) {
         this.reviewText = reviewText;
         this.Rating = Rating;
         this.MovieName = MovieName;
-    }
-
-    
-
+		this.userID = userID;
+	}
 	public void getCustomer() {
 		// TODO - implement Review.getCustomer
-		throw new UnsupportedOperationException();
+		return this.Customer;
 	}
-
     public int getRating() {
 		// TODO - implement Movie.getRating
-        return this.Rating;
-		
+        return this.Rating;	
 	}
-
-    public string getReview() {
+    public String getReview() {
 		// TODO - implement Movie.getReview
         return this.reviewText;
 	}
-    
-    public string getMovieName() {
-           
+    public String getMovieName() {      
         return this.MovieName;
     }
+	public void rottenTomatoes(){
+		System.out.println("Enter your name: ");
+		Scanner scanner = new Scanner(System.in);
+		String userID = scanner.nextLine();
 
-}
+		System.out.println("Enter your rating: ");
+		Scanner scanner1 = new Scanner(System.in);
+		int Rating = scanner1.nextInt();
+		
+		System.out.println("Enter your review: ");
+		Scanner scanner2 = new Scanner(System.in);
+		String reviewText = scanner2.nextLine();
+	}
