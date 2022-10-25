219900b1542ff07b70b8ca565a91ad952c6eb0ca
kopikia <100225153+kopikia@users.noreply.github.com>
2022-10-25 15:10:52 +0800

    Update Staff.java

--- Project/Staff.java
+++ Project/Staff.java
@@ -1,12 +1,27 @@
-public class Staff {
+public class Staff extends User {
 
 	private String Title;
 	private int StaffID;
 	private boolean Shift;
 
-	public void addMovieSchedule() {
-		// TODO - implement Staff.addMovieSchedule
-		throw new UnsupportedOperationException();
+	//String eMail[]= {"abc@outlook.com","qwer@hotmail.com", "xyz@gmail.com","pork@outlook.com", "qaz@gmail.com"};
+	//String pAss[]= {"1234abc","dbzy23","qwe345","tajey10985","prke1956"};
+	
+	public Staff(String Title, int StaffID, boolean Shift, String Name, String Email, String Password) {
+		super(Name,Email,Password);
+		this.Title=Title;
+		this.StaffID=StaffID;
+		this.Shift=Shift;
 	}
-
+	
+	
+	
+	
+	
+	public void addMovieSchedule() {
+		
+	}
+	
+	
+	
 }
