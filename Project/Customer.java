7395e2caae44955f94ce6579f38225f189340ace
JarrelNT <113711841+JarrelNT@users.noreply.github.com>
2022-11-01 17:20:59 +0800

    Update Customer.java

--- Project/Customer.java
+++ Project/Customer.java
@@ -1,7 +1,15 @@
+
 public class Customer extends User {
 
 	private String UserID;
 	private String typeOfCustomer;
+	private ArrayList<Ticket> Ticket=new ArrayList<Ticket>();//Creating arraylist
+	private MovieSchedule Movie = getMovie();
+	private Movie MovieTitle = getMovieTitle();
+	
+	
+	
+
 
 	public Customer(String Name, String Email, String Password, String typeOfCustomer) {
 		super(Name, Email, Password);
@@ -17,4 +25,25 @@ public class Customer extends User {
 		return this.typeOfCustomer;
 	}
 	
+
+	public MovieSchedule getMovie() {
+		return getMovie();
+	}
+
+	public Movie getMovieTitle() {
+		return getMovieTitle();
+	}
+
+
+	public void bookingHistory() {
+		System.out.println("You have previuosly watched: " );
+	
+		for (int i = 0; i < Ticket.size(); i++) {
+			System.out.println(i + 1 + ". " + Ticket.get(i).getMovieTitle());
+		} 
+	}
+	
+	
+
 }
+
