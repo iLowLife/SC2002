b444ad89a7f80378ad15ec373a5f626ae4538b5e
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-11-01 16:21:54 +0800

    Update MovieSchedule.java

--- Project/MovieSchedule.java
+++ Project/MovieSchedule.java
@@ -1,7 +1,55 @@
 public class MovieSchedule {
-
+		//XXXYYYYMMDDhhmm (Y : year, M : month, D : day, h : hour, m : minutes, XXX : cinema code in letters)
 	private String Movie;
-	private int Room;
+	private int RoomID;
 	private String ShowTime;
+	private String CinemaID;
+	private Room Room; //1 movie schedule object will create 1 new room object
 
+	public MovieSchedule(String Movie, int RoomID,String CinemaID, String ShowTime) {
+		this.Movie = Movie;
+		this.RoomID = RoomID;
+		this.ShowTime = ShowTime; //"YYYYMMDDhhmm"
+		this.CinemaID = CinemaID; //"XXX"
+		this.Room = new Room(RoomID);	//create a new room object
+	}
+	//Getter
+	public String getMovie() {
+		return this.Movie;
+	}
+
+	public int getRoomID() {
+		return this.RoomID;
+	}
+
+	public String getShowTime() {
+		return this.ShowTime;
+	}
+
+	public String getCinemaID() {
+		return this.CinemaID;
+	}
+
+	public Room getRoom() {
+		return this.Room;
+	}
+
+	//Setter
+	public void setMovie(String Movie) {
+		this.Movie = Movie;
+	}
+
+	public void setRoom(int RoomID) {
+		this.RoomID = RoomID;
+	}
+
+	public void setShowTime(String ShowTime) {
+		this.ShowTime = ShowTime;
+	}
+
+	public void checkConflict(){
+		// TODO - implement MovieSchedule.checkConflict
+
+		throw new UnsupportedOperationException();
+	}
 }
