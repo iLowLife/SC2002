0898df2acef018c3e7970685c6199f147cf8ddba
JarrelNT <113711841+JarrelNT@users.noreply.github.com>
2022-11-01 17:22:46 +0800

    Update Ticket.java

--- Project/Ticket.java
+++ Project/Ticket.java
@@ -1,3 +1,4 @@
+
 public class Ticket {
 
 	private int TicketID;
@@ -6,5 +7,22 @@ public class Ticket {
 	private String MovieSchedule;
 	private boolean Used;
 	private String UserEmail;
+	// private MovieSchedule Movie = getMovie();
+	// private Movie MovieTitle = getMovieTitle();
+
+	public String getMovieSchedule() {
+		return this.MovieSchedule;
+	}
+
+	public MovieSchedule getMovie() {
+		return getMovie();
+	}
+
+	public Movie getMovieTitle() {
+		return getMovieTitle();
+	}
+	
+
+	
 
 }
