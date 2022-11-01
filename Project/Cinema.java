ef4764dbfab1c960d64e9e67bdc4e690bed0c2eb
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-11-01 16:23:23 +0800

    Update Cinema.java

--- Project/Cinema.java
+++ Project/Cinema.java
@@ -1,18 +1,16 @@
-//package MovieBooking;
+import java.util.ArrayList;
 
 public class Cinema {
 	int CinemaID;
 	String location;
 	int NumOfRoom = 3;
-	private Room[] Room = new Room[NumOfRoom];
+	private ArrayList<Room> Room=new ArrayList<Room>();//Creating arraylist
 	
 	Cinema(int CinemaID){
 		this.CinemaID = CinemaID;
-		//this.NumOfRoom = NumOfRoom;
-		/*for (int i=0; i<NumOfRoom; i++) {
-			this.getRoom()[i] = new Room(i);
-		}*/
-
+		for (int i=0; i<NumOfRoom; i++) {
+			Room.add(new Room(i));	//Create Room with its ID
+		}
 	}
 	
 	int getCinemaID() {
@@ -27,20 +25,32 @@ public class Cinema {
 		this.location = location;
 	}
 	
-	void setNumOfRoom(int NumOfRoom) {
+	/*void setNumOfRoom(int NumOfRoom) {
 		this.NumOfRoom = NumOfRoom;
 		this.Room = new Room[NumOfRoom];
 		for(int i=0;i<NumOfRoom;i++) {
 			this.Room[i] = new Room(i);
 		}
-	}
+	}*/
 	
 	String getLocation() {
 		return this.location;
 	}
 
-	public Room[] getRoom() {
-		// TODO Auto-generated method stub
+	public ArrayList<Room> getRoom() {
 		return Room;
 	}
+
+	public void setRoom(ArrayList<Room> room) {
+		Room = room;
+	}
+
+	public void setCinemaID(int cinemaID) {
+		CinemaID = cinemaID;
+	}
+
+	public void setNumOfRoom(int numOfRoom) {
+		NumOfRoom = numOfRoom;
+	}
+
 }
